0d3e457fdf4bac8f0570e85765a316daf746457c
Paula <psanzilundain@gmail.com>
2024-03-20 10:07:46 +0100

    styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
+import '../scss/components/Header.scss';
 function Header() {
   return (
-    <>
+    <div className='header'>
       <h1>Country Info App</h1>
       <p>
         Explore information about countries, capitals, and flags. Add new
         countries and filter through the list!
       </p>
-    </>
+    </div>
   );
 }
 
